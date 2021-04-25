cd168a973d6423ecbd481cc7e58cb8615f453c85
Len Mahaffey <len.mahaffey@gmail.com>
2021-04-24 19:33:31 -0600

    Copyright partial view added

---
+++ ConferenceManager/Components/Copyright.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ConferenceManager.Components
+{
+    public class Copyright : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            int currentYear = DateTime.Today.Year;
+            return View(CopyrightOptions.Path, currentYear);
+        }
+    }
+}
---
+++ ConferenceManager/Components/_PartialViewPath.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferenceManager.Components
+{
+    public class CopyrightOptions
+    {
+        public const string Path = "~/Views/Shared/Components/Copyright/Copyright.cshtml";
+    }
+}
