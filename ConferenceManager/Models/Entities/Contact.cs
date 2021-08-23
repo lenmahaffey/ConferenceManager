f435ec1b60078a1a8b57ebce5c6067864cc2d9ed
Len Mahaffey <len.mahaffey@gmail.com>
2021-08-23 16:50:36 -0600

    begin contact class

--- ConferenceManager/Models/Entities/Contact.cs
+++ ConferenceManager/Models/Entities/Contact.cs
@@ -15,5 +15,13 @@ namespace ConferenceManager.Models.Entities
         [StringLength(50)]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set;}
+        public string Country { get; set; }
+
     }
 }
